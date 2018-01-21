05b8d9c8e3472bdd80c20a1b86c9f19a0f08dc7b
Orion Weldon <orion@Orions-MacBook-Pro.local>
2018-01-21 13:32:32 -0800

    reworked inputs, adds correctly

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,36 +4,38 @@ class Calculator extends Component {
   constructor(props){
     super(props)
     this.state = {
-      sum: 0,
-      num1: "",
-      num2: ""
-   };
-   this.addNum1 = this.addNum1.bind(this);
-   this.addNum2 = this.addNum2.bind(this);
- }
+              sum: 0
+      }
 
+      this.createTotal = this.createTotal.bind(this);
+      this.handleChange= this.handleChange.bind(this);
+    }
 
- addNum1(event){
-   let num =  Number(event.target.value)
-   let temp =  Number(this.state.sum) + num
-   this.setState({sum: temp, num1: num});
- }
+    handleChange(e){
+      let newNum = parseInt(e.target.value);
 
-  addNum2(event){
-   let num =  Number(event.target.value)
-   let temp =  Number(this.state.sum) + num
-   this.setState({sum: temp, num2: num});
- }
+      this.setState({[e.target.name]: newNum });
+    }
+
+  createTotal(event){
+    event.preventDefault();
+    
+    let num1 = this.state.num1;
+    let num2 = this.state.num2 ;
+    this.setState({ sum: num1 + num2 });
+  }
 
   render() {
     return(
       <div className="container">
         <h1>Add with React!</h1>
         <div className="add">
-            <input type="number" value={this.state.num1} onChange={this.addNum1} />
-            <span>+</span>
-            <input type="number" value={this.state.num2} onChange={this.addNum2} />
-            <input type="submit" value="=" />
+            <form onSubmit={this.createTotal}>
+              <input type="number" name="num1" onChange={this.handleChange} />
+              <span>+</span>
+              <input type="number" name="num2" onChange={this.handleChange} />
+              <input type="submit" value="=" />
+            </form>
           <h3>{this.state.sum}</h3>
         </div>
       </div>
