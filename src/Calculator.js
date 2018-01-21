7f7b1776d4e0364de4eae41d5670e27f15662eb2
Orion Weldon <orion@Orions-MacBook-Pro.local>
2018-01-21 13:41:43 -0800

    added reset button

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,7 +8,8 @@ class Calculator extends Component {
       }
 
       this.createTotal = this.createTotal.bind(this);
-      this.handleChange= this.handleChange.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.reset = this.reset.bind(this);
     }
 
     handleChange(e){
@@ -17,9 +18,15 @@ class Calculator extends Component {
       this.setState({[e.target.name]: newNum });
     }
 
+    reset(){
+      this.setState({ sum: 0 });
+      this.setState({ num1: " " });
+      this.setState({ num2: " " });
+    }
+
   createTotal(event){
     event.preventDefault();
-    
+
     let num1 = this.state.num1;
     let num2 = this.state.num2 ;
     this.setState({ sum: num1 + num2 });
@@ -31,12 +38,13 @@ class Calculator extends Component {
         <h1>Add with React!</h1>
         <div className="add">
             <form onSubmit={this.createTotal}>
-              <input type="number" name="num1" onChange={this.handleChange} />
+              <input type="number" name="num1" value={this.state.num1} onChange={this.handleChange} />
               <span>+</span>
-              <input type="number" name="num2" onChange={this.handleChange} />
+              <input type="number" name="num2" value={this.state.num2} onChange={this.handleChange} />
               <input type="submit" value="=" />
             </form>
           <h3>{this.state.sum}</h3>
+          <button onClick={this.reset}> Clear Calculator </button>
         </div>
       </div>
     );
